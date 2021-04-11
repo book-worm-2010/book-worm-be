1fb802a9ceb44e81d9428212896b7547a5b037eb
Catherine Dean <catherinemdean15@gmail.com>
2021-04-11 12:48:57 -0600

    create endpoint to create bookmark

---
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::BookmarksController < ApplicationController
+  def create
+    bookmark = Bookmark.new(bookmark_params)
+    if bookmark.save
+      render json: BookmarkSerializer.new(bookmark), status: :created
+    else
+      render json: { 'error' => {} }, status: 404
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.permit(:student_book_id, :date, :minutes, :page_number)
+  end
+end
