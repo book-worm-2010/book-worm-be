b91d6d475e868c2e326d0d7f026bc2edb68e31df
ninesky00 <jiangzho@gmail.com>
2021-04-13 15:14:52 -0500

    fix typo
    Co-authored-by: Catherine Dean <69832134+catherinemdean15@users.noreply.github.com>

--- db/migrate/20210413193453_changecolumnname.rb
+++ db/migrate/20210413193453_changecolumnname.rb
@@ -1,5 +1,5 @@
 class Changecolumnname < ActiveRecord::Migration[6.1]
   def change
-    rename_column :student_books, :review_comment, :review_comment
+    rename_column :student_books, :reivew_comment, :review_comment
   end
 end
