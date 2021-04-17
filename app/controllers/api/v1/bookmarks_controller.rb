97fd9339c9c33b3629ed29a590d05794c45b0851
Catherine Dean <catherinemdean15@gmail.com>
2021-04-17 16:38:32 -0600

    update bookmark requests to include student and book instead of student_book

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -1,16 +1,18 @@
 class Api::V1::BookmarksController < ApplicationController
   def create
+    student_book = StudentBook.where(student_id: params[:student_id], book_id: params[:book_id]).first
     bookmark = Bookmark.new(bookmark_params)
+    bookmark.update(student_book_id: student_book.id)
     if bookmark.save
       render json: BookmarkSerializer.new(bookmark), status: :created
     else
-      render json: { 'error' => 'must include minutes' }, status: 404
+      render json: { 'error' => 'something went wrong, or must include page number' }, status: 404
     end
   end
 
   private
 
   def bookmark_params
-    params.permit(:student_book_id, :date, :minutes, :page_number)
+    params.permit(:date, :minutes, :page_number)
   end
 end
